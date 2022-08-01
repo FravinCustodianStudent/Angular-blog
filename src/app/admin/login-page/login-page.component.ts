cd482d614edcc233fa8314aa76fe1a0749510917
FravinCustodianStudent <91590998+FravinCustodianStudent@users.noreply.github.com>
2022-08-01 22:31:36 +0300

    Login service update

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validator, Validators} from "@angular/forms";
 import {User} from "../shared/interfaces";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 import {AuthService} from "../shared/services/auth.service";
 
 @Component({
@@ -12,10 +12,18 @@ import {AuthService} from "../shared/services/auth.service";
 export class LoginPageComponent implements OnInit {
 
   form!:FormGroup
-  constructor(private auth:AuthService,
-               private router:Router) { }
+  submited: boolean = false;
+  message!:string
+  constructor(public auth:AuthService,
+               private router:Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((params:Params)=>{
+      if (params['loginAgain']){
+        this.message = "Login again"
+      }
+    })
     this.form = new FormGroup({
       email:new FormControl(null,[Validators.required,Validators.email]),
       password:new FormControl(null,[
@@ -29,13 +37,18 @@ export class LoginPageComponent implements OnInit {
     if(this.form.invalid){
       return
     }
+    this.submited=true;
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password,
+      returnSecureToken: null!
     }
     this.auth.login(user).subscribe(()=>{
       this.form.reset()
+      this.submited=false;
       this.router.navigate(['/admin','dashboard'])
+    },()=>{
+      this.submited = false;
     })
   }
 }
--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,26 +1,63 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {User} from "../interfaces";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {FbAuthResponse, User} from "../interfaces";
 import {Router} from "@angular/router";
-import {Observable} from "rxjs";
+import {catchError, Observable, Subject, tap} from "rxjs";
+import {environment} from "../../../../environments/environment";
 
 @Injectable()
 export class AuthService {
+  public error$: Subject<string> = new Subject<string>()
 
-  get token():string{
-    return ''
+  get token():string | null{
+      const exp = new Date(localStorage.getItem('fb-token-exp')!);
+      if (new Date()>exp){
+        this.logout()
+        return null
+      }
+    return localStorage.getItem('fb-token')
   }
   constructor(private http:HttpClient) { }
   login(user:User):Observable<any>{
-  return   this.http.post('',user)
+    user.returnSecureToken= true;
+
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`,user)
+    .pipe(
+      // @ts-ignore
+      tap(this.setToken),
+      catchError(this.handleError.bind(this))
+    )
   }
   logout(){
+    this.setToken(null)
+  }
+  private handleError(error:HttpErrorResponse) : any{
+    const {message} = error.error.error
+
+      switch (message){
+        case 'EMAIL_NOT_FOUND':
+          this.error$.next('Email not found')
+          break;
+        case 'INVALID_EMAIL':
+          this.error$.next('Invalid email')
+          break;
+        case 'INVALID_PASSWORD':
+          this.error$.next('Invalid Password')
+          break;
+      }
 
   }
   isAuthenticate():boolean{
     return !!this.token;
   }
-  private setToken(){
+  private setToken(response:FbAuthResponse | null){
+    if (response){
+      const expDate = new Date(new Date().getTime() + +response.expiresIn*1000)
+      localStorage.setItem('fb-token',response.idToken)
+      localStorage.setItem('fb-token-exp',expDate.toString())
+    }else {
+      localStorage.clear()
+    }
 
   }
 }
